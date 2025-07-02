472507c2771487595421a09c25dc6006110bd8a6
ukubaykiflay <ukubaydirar6@gmail.com>
2025-07-02 10:54:40 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace package express
+{
+	 class program
+
+{
+	static void main(string[] args)
+	{
+		int package weight = 35
+		int package height = 8
+		int package width = 10
+		int package length = 9
+			if (package weight > 35 && package height > 8 && package width > 10 && package length > 9)
+			console.WriteLine("this package is very big to ship,")
+	}		
+			if" the package weight is greater than 35 and its height is greater than 8 and its width is greater than 10 and its length is greater than 9,
+			 the people they can not ship it"
+		Else
+		{
+		  console.writeline("this package is very good to ship")
+
+}
+        if(package weight=35 and its height=8 and its width=10 and its length=9)
+	   {
+	console.writeline("this package is good to ship")
+	int product= 35*8*10*9
+     console.writeline(product);
+	console.readline(25200)
+	int quotient = 25200 / 100;
+	console.writeline(quotient);
+	cosole.readline(252);
+	out put total=252
+
+	}
+if"the package weight is equal to 35 and its height is equal to 8 and its width is equal to 10 and its length is equal to 9 an its final cost $252 dollars then the users can ship it"
+}}
+	}
+	}
+	}
+
+}
+		     
+
+
+}
