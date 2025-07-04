a04b5b56b32f8c51a99f41fe3f9ef740a1cd6184
ukubaykiflay <ukubaydirar6@gmail.com>
2025-07-04 10:02:14 -0400

    Add files via upload

---
+++ Class1.cs 8+8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace package express
+{
+	public Class1()
+{
+	static void main(string[] args)
+	{ 
+		int package weight = 30;
+		int package height = 8;
+		int package width = 10;
+		int package length = 9;
+			if(package weight > 30 && package height > 8 && package width >10 && package length >9)
+			{
+			console.writeLine(" this package is too big to ship.")
+				if"the package weight is greater than 30 and its height is greater than 8 and its width is greater than 10 and its length is greater than 9 then the users can not ship it",
+					}
+		Else
+			{
+			console.writeLine("this package is too good to ship.");
+		}
+		if(package weight=30 && its height =8 && its width=10 && its length=9)
+			{
+			console.writeLine("this package too good to ship".);
+		}
+		int product = 30 * 8 * 10 * 9;
+		console.writeLine(product);
+		console.ReadLine();
+		int quotient = 21600 / 100;
+		console.writeLine(quotient);
+		console.ReadLine();
+		output total=216
+			if" the package weight is equal to 30 and its height is equal to 8 and its width is equal to 10 and its length is equal to 9 and its final price is $216 dollars the people they can ship it". 
+	}
+}
