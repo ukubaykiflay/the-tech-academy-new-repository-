fbcda75ae041dd7b795036753829fe21f21db6ff
ukubaykiflay <ukubaydirar6@gmail.com>
2025-07-17 20:42:00 -0400

    Add files via upload

---
+++ Class1.cs 10+10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Method Class 
+{
+	public Class MathOperations  
+	{
+	public statics int Multiply(int x, int y)
+{
+	return x * y;
+}
+static void Main(string[] args)
+{
+	int result = MathOperations.Multiply(4, 5)
+		Console.WriteLine($" the product is :{result}");
+}
+{
+public statics int Add(int x, int y)
+{
+	return x + y;
+}
+static void Main(strring[] args)
+{
+	int result = MathOperations.Add(4, 5);
+	Console.WriteLine($"The sum is : {result}") 
+
+
+
+	}
+}
